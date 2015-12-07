d8abeb6cd202aad522572aa38b73b851c6316001
jslvtr <jslvtr@gmail.com>
2015-12-07 17:44:53 +0000

    Removed cloud_sptheme from conf.py.

--- conf.py
+++ conf.py
@@ -30,9 +30,7 @@ import shlex
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax',
-	'cloud_sptheme.ext.index_styling',
-	'cloud_sptheme.ext.relbar_toc'
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
